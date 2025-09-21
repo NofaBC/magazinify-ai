d303beeb5c1662748418d98e80364820500d07a7
NofaBC <167025181+NofaBC@users.noreply.github.com>
2025-09-21 14:23:40 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-vite.config.jsimport { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react( )],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+  server: {
+    port: 5173,
+    host: true
+  }
 })
