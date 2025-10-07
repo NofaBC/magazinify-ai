3850fbe312fb4381ef41661c27c2ee519c4da72a
NofaBC <167025181+NofaBC@users.noreply.github.com>
2025-10-07 15:07:03 -0400

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/lib/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          "2xl": "1280px",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
